f36d05999a91c0d90aecdab16184480e5414a825
edumiguel <dudumfmonteiro@gmail.com>
2024-04-22 10:17:48 -0300

    Aula15

---
+++ aula15/TesteJava1.java
@@ -0,0 +1,5 @@
+
+        public class TesteJava1 {
+        public static void main (String[] args)
+            System.out.println("Teste 1 feito em Java")
+        }
---
+++ aula15/teste-arquivo2.py
@@ -0,0 +1,4 @@
+arq1 = open ("./nomes.txt", "w", encoding="utf-8")
+arq1.write("Jõao\n")
+arq1.write("Maria\n")
+arq1.write("José\n")
